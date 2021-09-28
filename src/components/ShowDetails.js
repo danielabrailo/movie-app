d7191586731a606ab7c5ed8216e986676dbe7265
Daniela Brailovsky <dbrailo@gmail.com>
2021-09-28 16:33:29 +0300

    Added the show details component

---
+++ src/components/ShowDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShowDetails = () =>{
+
+    return (
+        <>
+            <span className="mr-2">Show details</span>            
+        </>
+    );
+};
+
+export default ShowDetails;
